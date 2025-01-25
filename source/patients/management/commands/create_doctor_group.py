13204488b4f82ecfd0e284d356a0ddb89335cd46
Arthur <arturkuliev2@gmail.com>
2025-01-26 04:09:30 +0600

    doctor role creation

---
+++ source/patients/management/commands/create_doctor_group.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+from .models import Patient
+
+
+class Command(BaseCommand):
+    help = 'Создание группы doctor и добавление прав модели Patient'
+
+    def handle(self, *args, **kwargs):
+        group, created = Group.objects.get_or_create(name='doctor')
+
+        if created:
+            self.stdout.write(self.style.SUCCESS('Группа doctor успешно создана!'))
+        else:
+            self.stdout.write(self.style.SUCCESS('Группа doctor уже существует!'))
+
+        content_type = ContentType.objects.get_for_model(Patient)
+        permissions = Permission.objects.filter(content_type=content_type)
+        group.permissions.add(*permissions)
+
+        self.stdout.write(self.style.SUCCESS('Все права для модели Patient успешно добавлены группе doctor!'))
